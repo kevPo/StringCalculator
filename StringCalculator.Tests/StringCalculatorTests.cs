8afd96ae55a6e885f98416cacfca091e54713813
Kevin Poston <kposton@qualtrax.com>
2014-02-11 09:48:07 -0500

    Passed first test

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void TestAddEmptyString()
+        {
+            var calculator = new StringCalculator();
+            Assert.That(calculator.Add(""), Is.EqualTo(0));
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(String input)
+        {
+            return 0;
+        }
+    }
+}
